75bf7c51e85a6fd75a23360a9ce8d6601407edaa
Zaid-Waghoo <se>
2024-08-26 17:57:17 +0000

    s

--- mbench/__init__.py
+++ mbench/__init__.py
@@ -1,25 +1,19 @@
 # SPDX-FileCopyrightText: 2024-present Sebastian Peralta <sebastian@mbodi.ai>
 #
 # SPDX-License-Identifier: apache-2.0
-<<<<<<< HEAD
 
 from typing import Literal
-from .profile import profileme, profile, profiling, main, FunctionProfiler
-from functools import wraps
+
+from funkify import funkify
+
+from .profile import main, profile, profileme, profiling
 
 __all__ = ["profileme", "profiling", "profile", "mbench"]
 
-@wraps(profileme)
+@funkify
 def mbench(mode: Literal["caller", "callee"] = "caller"):
     """Profile the code"""
     return profileme(mode)
 
 if __name__ == '__main__':
     main()
-=======
-from .profile import profileme
-from .profile import profiling
-from .profile import profile
-
-__all__ = ["profileme", "profiling", "profile"]
->>>>>>> 9caddfa4a3c5301a64f24efe60382ed55af85c9c
